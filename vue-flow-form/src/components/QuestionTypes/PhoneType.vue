5e1017009ccb9d5833fea2ac767483a60937a2d3
mooremyles23 <35779501+mooremyles23@users.noreply.github.com>
2021-12-01 12:41:28 -0500

    Add files via upload

---
+++ vue-flow-form/src/components/QuestionTypes/PhoneType.vue
@@ -0,0 +1,43 @@
+<template>
+  <the-mask
+    ref="input"
+    v-bind:mask="question.mask"
+    v-bind:value="value"
+    type="tel"
+    v-bind:masked="false"
+    v-bind:placeholder="placeholder"
+    v-on:keydown.native="onKeyDown"
+    v-on:keyup.native="onChange"
+    v-on:focus.native="setFocus"
+    v-on:blur.native="unsetFocus"
+    v-on:keyup.native.enter.prevent="onEnter"
+    v-on:keyup.native.tab.prevent="onEnter"
+  />
+</template>
+
+<script>
+  /*
+    Copyright (c) 2020 - present, DITDOT Ltd. - MIT Licence
+    https://github.com/ditdot-dev/vue-flow-form
+    https://www.ditdot.hr/en
+  */
+
+  import BaseType from './BaseType.vue'
+  import LanguageModel from '../../models/LanguageModel'
+  import { QuestionType } from '../../models/QuestionModel'
+  import TheMask from 'vue-the-mask/src/component'
+  
+  export default {
+    extends: BaseType,
+    components: {
+      TheMask
+    },
+    name: QuestionType.Phone,
+    data() {
+      return {
+        inputType: 'tel', 
+        canReceiveFocus: true
+      }
+    }
+  }
+</script>
---
+++ vue-flow-form/src/components/QuestionTypes/TextType.vue
@@ -0,0 +1,38 @@
+<template>
+  <input
+    ref="input"
+    v-bind:type="inputType"
+    v-bind:value="value"
+    v-bind:required="question.required"
+    v-on:keydown="onKeyDown"
+    v-on:keyup="onChange"
+    v-on:keyup.enter.prevent="onEnter"
+    v-on:keyup.tab.prevent="onEnter"
+    v-on:focus="setFocus"
+    v-on:blur="unsetFocus"
+    v-bind:placeholder="placeholder"
+  />
+</template>
+
+<script>
+  /*
+    Copyright (c) 2020 - present, DITDOT Ltd. - MIT Licence
+    https://github.com/ditdot-dev/vue-flow-form
+    https://www.ditdot.hr/en
+  */
+
+  import BaseType from './BaseType.vue'
+  import { QuestionType } from '../../models/QuestionModel'
+  import LanguageModel from '../../models/LanguageModel'
+
+  export default {
+    extends: BaseType,
+    name: QuestionType.Text,
+    data() {
+      return {
+        inputType: 'text', 
+        canReceiveFocus: true
+      }
+    }
+  }
+</script>
